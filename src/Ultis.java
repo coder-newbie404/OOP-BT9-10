4935a788372c7514d0b1b5a77696ee4b181652f9
coder-newbie404 <74915494+coder-newbie404@users.noreply.github.com>
2021-11-10 12:49:56 +0700

    1

--- src/Ultis.java
+++ src/Ultis.java
@@ -1,8 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
 
 public class Ultis {
     public static List<Integer> readFile(String s) throws FileNotFoundException {
@@ -15,11 +15,35 @@ public class Ultis {
         return lst;
     }
 
+    public static void writeFile(List<PhanTu<Integer>> lst) {
+        try {
+            File myObj = new File("out.txt");
+            if (myObj.createNewFile()) {
+                FileWriter myWriter = new FileWriter("out.txt");
+                for (PhanTu<Integer> i : lst) {
+                    myWriter.write(i.getT());
+                }
+                myWriter.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void sortPhanTu(List<PhanTu<Integer>> lst) {
+    }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        List<Integer> numArr = Ultis.readFile(args[0]);
+    public static void main(String[] args) throws IOException {
+        List<Integer> exm = Ultis.readFile(args[0]);
+        Collections.sort(exm);
 
-        PhanTu<Integer> test = new PhanTu<>();
+        List<PhanTu<Integer>> test2 = new ArrayList<>();
+        while (exm.size() != 0) {
+            PhanTu<Integer> test = new PhanTu<>();
+            test.setT(exm.remove(0));
+            test2.add(test);
+        }
+
+        Ultis.writeFile(test2);
     }
 }
